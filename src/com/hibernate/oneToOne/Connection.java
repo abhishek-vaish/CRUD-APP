ce52bbf281342d4c339769226b7a61a007184795
Abhishek Vaish <abhishek.vaish_int18@gla.ac.in>
2020-05-21 10:45:39 +0530

    connection class created

---
+++ src/com/hibernate/oneToOne/Connection.java
@@ -0,0 +1,12 @@
+/*
+ *   Created by IntelliJ IDEA Ultimate, 2020
+ *   User: Abhishek
+ *   Date: 21-05-2020
+ *   Time: 10:45 AM
+ */
+
+
+package com.hibernate.oneToOne;
+
+public class Connection {
+}
