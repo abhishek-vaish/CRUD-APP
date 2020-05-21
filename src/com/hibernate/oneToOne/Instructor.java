2117251849d46f10cfaecc7ed9fb4407b70efb54
Abhishek Vaish <abhishek.vaish_int18@gla.ac.in>
2020-05-21 11:04:09 +0530

    create Instructor class

---
+++ src/com/hibernate/oneToOne/Instructor.java
@@ -0,0 +1,78 @@
+/*
+ *   Created by IntelliJ IDEA Ultimate, 2020
+ *   User: Abhishek
+ *   Date: 21-05-2020
+ *   Time: 10:47 AM
+ */
+
+
+package com.hibernate.oneToOne;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "instructor")
+public class Instructor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "firstname")
+    private String firstname;
+    @Column(name = "lastname")
+    private String lastname;
+    @Column(name = "email")
+    private String email;
+
+    public Instructor() {
+    }
+
+    public Instructor(String firstname, String lastname, String email) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Instructor{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
